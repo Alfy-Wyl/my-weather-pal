8629220075e5b16cdf5cdb8a42108b64a3d08aeb
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-06 10:51:05 +0000

    added code to app.js file

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Define variables for UI using ID
+const searchInput = document.getElementById("search-input")
+const searchBtn  = document.getElementById("search-button")
+
+// Define variables for weather results
+const todayLocationName = document.getElementById("today-location-name")
+const todayTemp = document.getElementById("today-temp")
+const todayWind = document.getElementById("today-wind")
+const todayHumidity = document.getElementById("today-humidity")
+
