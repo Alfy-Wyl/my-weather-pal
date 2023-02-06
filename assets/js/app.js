c10b3cec5ed54f577d474b8b2e1353b6038b8882
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-06 10:54:55 +0000

    added code to app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,17 @@ const todayTemp = document.getElementById("today-temp")
 const todayWind = document.getElementById("today-wind")
 const todayHumidity = document.getElementById("today-humidity")
 
+// Define variables for search history that will be appended
+const forecastContainer = document.getElementById("forecast")
+const historyContainer = document.getElementById("history")
+const historyList = JSON.parse(localStorage.getItem("locations") || '[]') 
+
+const handleSearch=async(place)=>{
+    
+    // Set conditional to handle Search Button click by user without any input in placeholder
+    if(place?.length<= 0){
+        alert("Please enter city name before searching!")
+        return
+    }
+
+}
